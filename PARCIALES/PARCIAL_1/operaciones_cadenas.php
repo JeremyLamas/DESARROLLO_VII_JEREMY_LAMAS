c0ee34dcbf0da7dd5a49db29093c6f66fa0abc91
JeremyLamas <jeremylamas6@gmail.com>
2025-09-20 12:31:39 -0500

    Paso 1: Implementación de la clase Libro y otros cambios

--- PARCIALES/PARCIAL_1/operaciones_cadenas.php
+++ PARCIALES/PARCIAL_1/operaciones_cadenas.php
@@ -1,4 +1,40 @@
 <?php
+function contar_palabras_repetidas($texto) {
 
+    $palabras = explode(" ", trim($texto));
 
+    $conteo = [];
+
+    foreach ($palabras as $palabra) {
+  
+        $palabra = strtolower(trim($palabra));
+
+        if ($palabra != "") {
+            if (isset($conteo[$palabra])) {
+                $conteo[$palabra]++;
+            } else {
+                $conteo[$palabra] = 1;
+            }
+        }
+    }
+
+    return $conteo;
+}
+
+function capitalizar_palabras($texto) {
+ 
+    $palabras = explode(" ", trim($texto));
+    $resultado = [];
+
+    foreach ($palabras as $palabra) {
+        if ($palabra != "") {
+            $primera = strtoupper(substr($palabra, 0, 1));
+            $resto = strtolower(substr($palabra, 1));
+            $resultado[] = $primera . $resto;
+        }
+    }
+
+    return implode(" ", $resultado);
+}
 ?>
+
